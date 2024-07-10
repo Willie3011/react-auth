b3319047dde7d2f515eb381e7a079d4cde0b9203
Buddynice34 <willemapril33@gmail.com>
2024-07-10 11:09:29 +0200

    Created a Simple router for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { Button } from "bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Signup from "./components/Auth/Signup/Signup"
+import ForgotPassword from "./components/Auth/ForgotPassword/ForgotPassword"
+import Login from "./components/Auth/Login/Login"
 
 function App() {
   
   return (
-    <>
-      <Button/>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={ <Signup/>} />
+        <Route path="/login" element={ <Login/>} />
+        <Route exact path="/forgot-password" element={ <ForgotPassword/>} />
+      </Routes>
+    </Router>
   )
 }
 
