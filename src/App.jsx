eb6c220a7c1eca3f11fbe2f6dee641d715fee689
Buddynice34 <willemapril33@gmail.com>
2024-07-10 11:54:22 +0200

    Created signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Signup from "./components/Auth/Signup/Signup"
 import ForgotPassword from "./components/Auth/ForgotPassword/ForgotPassword"
 import Login from "./components/Auth/Login/Login"
+import { Container } from "react-bootstrap"
 
 function App() {
-  
+
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={ <Signup/>} />
-        <Route path="/login" element={ <Login/>} />
-        <Route exact path="/forgot-password" element={ <ForgotPassword/>} />
-      </Routes>
-    </Router>
+    <Container className="d-flex justify-content-center align-items-center" style={{background: "#0005", height: "100vh", color: "white"}}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route exact path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </Container>
   )
 }
 
--- src/components/Auth/Signup/Signup.jsx
+++ src/components/Auth/Signup/Signup.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        setError("");
+        //Check if Password Matches 
+        if (password !== confirmPassword) {
+            setError("Passwords do not match");
+        }
+
+        
+    }
+
+    return (
+        <div className='d-flex flex-column align-items-center' style={{width: "100%"}}>
+            <Card className='p-4' style={{maxWidth: "400px", width: "100%"}}>
+                <h2 className='text-center mb-4'>Sign Up</h2>
+                {error && <Alert variant='danger'>{error}</Alert>}
+                <Card.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-2" controlId="Email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group className="mb-2" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+                        </Form.Group>
+                        <Form.Group className="mb-2" controlId="confirmPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control  className="mb-4" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirmn password" />
+                        </Form.Group>
+                        <Button variant='primary' type="submit" className='w-100'>Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className='w-100 d-flex align-items-center justify-content-center mt-2 gap-2'>
+                Already have an account ? <Link to="/login">Login</Link>
+            </div>
+        </div>
+    )
 }
 
 export default Signup
